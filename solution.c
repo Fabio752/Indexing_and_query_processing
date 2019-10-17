11ee222457c914a37695248f6a8412f5e6230932
Fabio Deo <fd5117@edge13.doc.ic.ac.uk>
2019-10-17 16:40:32 +0100

    first bugged version of a hash join

--- solution.c
+++ solution.c
@@ -1,10 +1,41 @@
 #include "solution.h"
+int hash(int value, int size){
+  return value % size;
+}
+int nextSlot(int current_slot, int size){
+  return (current_slot + 1) % size;
+}
+
+struct HashTableSlot {
+  bool is_occupied;
+  int sales_date;
+  int employee;
+};
 
 int Query1(struct Database* db, int managerID, int price) {
-  (void)db;        // prevent compiler warning about unused variable
-  (void)managerID; // prevent compiler warning about unused variable
-  (void)price;     // prevent compiler warning about unused variable
-  return 0;
+  HashTableSlot* hash_table[db->itemsCardinality + 1];
+  int touples_count = 0;
+  for (size_t i = 0; i < db->itemsCardinality; i++) {
+    ItemTuple& build_input = build[i];
+    int hash_value = hash(build_input.salesDate);
+    while (hash_table[hash_value].is_occupied) {
+      hash_value = nextSlot(hash_value);
+    }
+    hash_table[hash_value] = {true, build_input.sales_date, 
+                              build_input.employee};
+  }
+  for (size_t i = 0; i < db->ordersCardinality; i++) {
+    OrderTuple& probe_input = probe[i];
+    int hash_value = hash(probe_input.salesDate);
+    while (hash_table[hash_value].is_occupied && 
+           hash_table[hash_value] != probe_input.salesDate) {
+      hash_value = nextSlot(hash_value);
+    }
+    if (hash_table[hash_value].is_occupied) {
+      touples_count++;
+    }
+  }
+  return touples_count;
 }
 
 int Query2(struct Database* db, int discount, int date) {
